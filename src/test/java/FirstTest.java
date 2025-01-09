9e1265d8f4ef18c8c0a9af77732f751ccb86dac5
Stas163 <st.golovin91@gmail.com>
2025-01-09 19:43:28 +0400

    Add first test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,8 @@
+package src.test.java;
+
+public class FirstTest {
+    private static String msg = "Hello qa.guru!";
+    public static void main(String... args) {
+        System.out.println(msg);
+    }
+}
